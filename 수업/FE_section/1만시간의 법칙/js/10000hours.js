e39da390bc6256bfef275879955943ccbfaa23ba
hyoenji.choi <jh04022@naver.com>
2022-09-14 11:32:07 +0900

    9/14

--- 수업/FE_section/1만시간의 법칙/js/10000hours.js
+++ 수업/FE_section/1만시간의 법칙/js/10000hours.js
@@ -1,5 +1,12 @@
 // js file
+
 //input으로 받은 text화면에 띄우기
+const write = () => { 
+ 
+  
+  /*document.getElementsByClassName(".re-wanabe").text(txt_result)*/
+}
+document.querySelector('.btn-res').addEventListener("click",write)
 //버튼 누르면 모달창 나타나고 없어지기
 const open = () =>{
   document.querySelector(" .cont-modal").style.display="block";
@@ -10,10 +17,7 @@ const close = () =>{
 document.querySelector('.btn-go').addEventListener('click',open);
 document.querySelector('.cont-modal').addEventListener("click",close);
 
-const write = () => {
-  const result = document.querySelector(".cont-inp"); 
-  resultwanabe = result.querySelector(".txt-wannabe").value;
-  console.log(resultwanabe);
-  document.getElementsByClassName(".re-wanabe").text(resultwanabe)
-}
-document.querySelector('.btn-res').addEventListener('click',write)
+const txt_result = document.querySelector (".txt-wannabe ");
+txt= txt_result.querySelector('input').value;
+console.log(txt);
+console.log("dd");
